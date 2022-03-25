e57f9baca4cecbf79595bd114c03666417850d22
HwijooJang <jhj9469@naver.com>
2022-03-25 19:02:45 +0900

    commit

---
+++ src/algorithm/D03_25_1_2741.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class D03_25_1_2741 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=a ; i++) {
+			System.out.println(i);
+		}
+
+		// 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+	}
+
+}
